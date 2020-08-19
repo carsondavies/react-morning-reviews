a494eca7c86fe85bf68b2cf633d42dec4e8f474f
Carson Davies <carsondavies@Carsons-MBP.T-mobile.com>
2020-08-19 09:59:15 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,1 +1,44 @@
+// import React from 'react'
+// import './App.css'
 
+// export default function App(){
+  
+//   return(
+//     <div>
+//       <h1> The Most Amazing Movie List</h1>
+//     </div>
+//   )
+// }
+
+// import React, {Component} from 'react'
+// import './App.css'
+// import List from './List'
+// export default class App extends Component {
+//   constructor(props){
+//     super()
+//   }
+
+//   render(){
+//     return(
+//       <div className='App'>
+//         <h1 className='movie-list-title'>The Most Amazing Movie List</h1>
+//         <List />
+//       </div>
+//     )
+//   }
+
+// }
+
+import React from 'react'
+import './App.css'
+import List from './List'
+
+export default function App(){
+
+  return(
+    <div className='App'>
+      <h1 className='movie-list-title'>The Most Amazing Movie List</h1>
+      <List/>
+    </div>
+  )
+}
---
+++ src/List.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class List extends React.Component{
+  constructor(){
+    super()
+    
+    this.state = {
+      movies: []
+    }
+  }
+
+  render(){
+    return(
+      <div>I am the list Component</div>
+    )
+  }
+
+}
+
+export default List
